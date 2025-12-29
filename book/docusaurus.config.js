3756c2d98e678c652e4b383ce3a9de89601bb640
humaafatima <humaf1904@gmail.com>
2025-12-29 19:23:30 +0500

    updated docusaurus.config.js file

--- book/docusaurus.config.js
+++ book/docusaurus.config.js
@@ -12,12 +12,12 @@ const config = {
   url: 'https://humaafatima.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-Humanoid-Robotics-Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'humaafatima', // Usually your GitHub org/user name.
-  projectName: 'physical-ai-humanoid-robotics-book', // Usually your repo name.
+  projectName: 'humanoid-robotic-book-new-ai', // Usually your repo name.
   
 
   onBrokenLinks: 'throw',
